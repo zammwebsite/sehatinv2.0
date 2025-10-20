b48947c6aea3976bc48c1bc1c624184f8ba4a181
zammwebsite <zforzammm@gmail.com>
2025-10-20 17:58:01 +0700

    main.tsx

--- main.tsx
+++ main.tsx
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ErrorBoundary } from './components/ErrorBoundary';
 
+// Pastikan Tailwind dan CSS utama diimport agar style tampil
+import './index.css';
+
+// Ambil elemen root
 const rootElement = document.getElementById('root');
+
 if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+  throw new Error('Root element not found. Make sure your index.html has <div id="root"></div>');
 }
 
+// Buat root React 18+
 const root = ReactDOM.createRoot(rootElement);
+
 root.render(
   <React.StrictMode>
     <ErrorBoundary>
